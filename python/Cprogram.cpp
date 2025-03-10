91dd1ba4f66330eea39a477f43a620621cd68612
paddeyvreeti <vaddeypreeti@gmail.com>
2025-03-10 15:53:56 -0700

    new C program

---
+++ python/Cprogram.cpp
@@ -0,0 +1,15 @@
+// First C++ code!
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int flavor; 
+
+    cout << "Enter your favorite ice cream flavor: "; 
+    cin >> flavor; 
+    cout << "Your favorite flavor is " << flavor << endl; 
+
+    return 0;
+
+}
